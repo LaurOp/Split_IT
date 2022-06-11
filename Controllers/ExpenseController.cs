6ddd75e13682e72d5b8bf9f3971d3017b8eb5b47
LaurGen <oprealaur@yahoo.com>
2022-06-11 17:15:05 +0300

    half of ExpenseController

--- Controllers/ExpenseController.cs
+++ Controllers/ExpenseController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Split_IT.Entities;
+using Split_IT.Entities.DTOs;
 using Split_IT.Repositories.ExpenseRepository;
 using Split_IT.Repositories.GroupRepository;
 using Split_IT.Repositories.UserRepository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Split_IT.Controllers
 {
@@ -19,6 +23,61 @@ namespace Split_IT.Controllers
             _userRepository = userRepository;
             _groupRepository = groupRepository;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllExpenses()
+        {
+            var expenses = await _repository.GetAllExpenses();
+            var expToReturn = new List<ExpenseDTO>();
+
+            foreach (var ex in expenses)
+            {
+                expToReturn.Add(new ExpenseDTO(ex));
+            }
+
+            return Ok(expToReturn);
+        }
+
+        [HttpGet("user={user}")]
+        public async Task<IActionResult> GetAllExpensesByUserId(int user)
+        {
+            var allExp = await _repository.GetAllExpenses();
+            var expToReturn = new List<ExpenseDTO>();
+
+            foreach (var exp in allExp)
+            {
+                var itsUsers = exp.AmountsOwed;
+                foreach (var itsUser in itsUsers)
+                {
+                    if (itsUser.UserId == user)
+                    {
+                        expToReturn.Add(new ExpenseDTO(exp));
+                        break;
+                    }
+                }
+
+            }
+
+            return Ok(expToReturn);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateExpense(CreateExpenseDTO dto)
+        {
+            Expense newexpense = new Expense();
+            newexpense.totalAmount = dto.totalAmount;
+            newexpense.Group = dto.Group;
+            newexpense.GroupId = dto.GroupId;
+
+            _repository.Create(newexpense);
+
+            await _repository.SaveAsync();
+
+            return Ok(new ExpenseDTO(newexpense));
+
+        }
+
+
 
 
     }
