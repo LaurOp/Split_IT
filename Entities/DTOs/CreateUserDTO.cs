77c7625f5c2b94dff267189f744e2be7ea897859
LaurGen <oprealaur@yahoo.com>
2022-06-14 01:12:11 +0300

    all controllers

---
+++ Entities/DTOs/CreateUserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Split_IT.Entities.DTOs
+{
+    public class CreateUserDTO
+    {
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public float WalletFunds { get; set; }
+    }
+}
---
+++ Repositories/FriendshipRepository/FriendshipRepository.cs
@@ -0,0 +1,11 @@
+﻿using Split_IT.Data;
+using Split_IT.Entities.Models;
+using Split_IT.Repositories.GenericRepository;
+
+namespace Split_IT.Repositories.FriendshipManager
+{
+    public class FriendshipRepository : GenericRepository<Friendship>, IFriendshipRepository
+    {
+        public FriendshipRepository(ProjectContext context) : base(context) { }
+    }
+}
